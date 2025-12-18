bd9a70950b50b550ec839d5a87c073ca6d915cbd
THU5HAR <macbook@THUSHARs-MacBook-Pro.local>
2025-12-18 10:18:14 +0530

    update portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to match your repository name
+  base: '/Portfolio/', // Matches repository name
 })
 
